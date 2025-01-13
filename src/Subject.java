9fdea18f891ab70eac36a8d7e2e1c37de1804190
SAM Sokleap <leapngu2023@gmail.com>
2025-01-13 21:36:33 +0700

    Task3,4,5

--- src/Subject.java
+++ src/Subject.java
@@ -5,16 +5,16 @@
  * Modified: 13 January 2025
  * Description: Lab 05
  * Functions:
- *      -getPrice(): Subject
- *      -getProgram(): Subject
- *      -getName(): Subject
- *      -getCredits(): Subject
+ * -getPrice(): Subject
+ * -getProgram(): Subject
+ * -getName(): Subject
+ * -getCredits(): Subject
  * Variables:
- *      -price: Subject
- *      -PROGRAM: Subject
- *      -name: Subject
- *      -credits: Subject
- * */
+ * -price: Subject
+ * -PROGRAM: Subject
+ * -name: Subject
+ * -credits: Subject
+ */
 public class Subject {
 
     private static final double price;
@@ -46,6 +46,13 @@ public class Subject {
 
     public int getCredits() {
         return credits;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Subject name cannot be null or empty.");
+        }
+        this.name = name;
     }
 
     public static void main(String[] args) {
